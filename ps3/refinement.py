c73947bf3916cc835069d6f1ba88caf0f5078b12
muyaho-universe <kimdaesk@gmail.com>
2025-05-19 20:29:30 +0900

    Update similarity

--- ps3/refinement.py
+++ ps3/refinement.py
@@ -1,5 +1,5 @@
 from effect import Effect
-from pyvex.expr import IRExpr, Binop, Const, Unop, Load
+from pyvex.expr import IRExpr, Binop, Const, Unop, Load, IRConst
 from symbol_value import AnySymbol, RegSymbol, ReturnSymbol, MemSymbol
 from inspect_info import InspectInfo
 
@@ -62,32 +62,6 @@ def traverse_expr(expr: IRExpr):
 
 
 def similarity(vuln: Effect, patch: Effect, type) -> int:
-    # if type(vuln) != type(patch):
-    #     return 0
-
-    # # Call: compare function arguments
-    # if isinstance(vuln, Effect.Call) and isinstance(patch, Effect.Call):
-    #     score = 1  # structure match
-    #     for a, b in zip(vuln.args, patch.args):
-    #         score += similarity_expr(a, b)
-    #     return score
-
-    # # Condition
-    # if isinstance(vuln, Effect.Condition) and isinstance(patch, Effect.Condition):
-    #     return 1 + similarity_expr(vuln.expr, patch.expr)
-
-    # # Return
-    # if isinstance(vuln, Effect.Return) and isinstance(patch, Effect.Return):
-    #     return 1 + similarity_expr(vuln.expr, patch.expr)
-
-    # # Put
-    # if isinstance(vuln, Effect.Put) and isinstance(patch, Effect.Put):
-    #     return 1 + similarity_expr(vuln.dst, patch.dst) + similarity_expr(vuln.src, patch.src)
-
-    # # Store
-    # if isinstance(vuln, Effect.Store) and isinstance(patch, Effect.Store):
-    #     return 1 + similarity_expr(vuln.addr, patch.addr) + similarity_expr(vuln.data, patch.data)
-
     if type == "call":
         score = 1
         for a, b in zip(vuln.args, patch.args):
@@ -99,8 +73,11 @@ def similarity(vuln: Effect, patch: Effect, type) -> int:
         return 1 + similarity_expr(vuln.expr, patch.expr)
     elif type == "put":
         return 1 + similarity_expr(vuln.reg, patch.reg) + similarity_expr(vuln.expr, patch.expr)
-
-    return 0
+    elif type == "store":
+        return 1 + similarity_expr(vuln.addr, patch.addr) + similarity_expr(vuln.expr, patch.expr)
+    else:
+        print(f"Unknown effect type: {type}")
+        return 0
 
 def similarity_expr(a, b) -> int:
 
@@ -108,29 +85,50 @@ def similarity_expr(a, b) -> int:
         return 0
 
     # Constants
-    if isinstance(a, Const) and isinstance(b, Const):
-        return int(a.value == b.value)
+    elif isinstance(a, Const) and isinstance(b, Const):
+        
+        return similarity_expr(a.con, b.con)
 
     # Symbolic registers
-    if isinstance(a, RegSymbol) and isinstance(b, RegSymbol):
-        return int(a.reg == b.reg)
+    elif isinstance(a, RegSymbol) and isinstance(b, RegSymbol):
+        print(f"RegSymbol: {a}, {b}")
+        return 1
 
     # Binary operations
-    if isinstance(a, Binop) and isinstance(b, Binop):
+    elif isinstance(a, Binop) and isinstance(b, Binop):
+        print(f"Binop: {a}, {b}")
         if a.op != b.op:
             return 0
         return 1 + similarity_expr(a.args[0], b.args[0]) + similarity_expr(a.args[1], b.args[1])
 
     # Memory load
-    if isinstance(a, Load) and isinstance(b, Load):
+    elif isinstance(a, Load) and isinstance(b, Load):
+        print(f"Load: {a}, {b}")
         return 1 + similarity_expr(a.addr, b.addr)
 
     # Unary operations
-    if isinstance(a, Unop) and isinstance(b, Unop):
+    elif isinstance(a, Unop) and isinstance(b, Unop):
+        print(f"Unop: {a}, {b}")
         if a.op != b.op:
             return 0
-        return 1 + similarity_expr(a.arg, b.arg)
+        return 1 + similarity_expr(a.args[0], b.args[0])
+    
+    # Memory symbols
+    elif isinstance(a, MemSymbol) and isinstance(b, MemSymbol):
+        print(f"MemSymbol: {a}, {b}")
+        return 1 + similarity_expr(a.address, b.address)
+    
+    # IRConst symbols
+    elif isinstance(a, IRConst) and isinstance(b, IRConst):
+        print(f"IRConst: {a}, {b}")
+        return 1
 
+    else:
+        print(f"Unknown expression type: {type(a)}")
+        print(f"Unknown expression type: {type(b)}")
+        print(f"Unknown expression: {a}")
+        print(f"Unknown expression: {b}")
+        exit(1)
     return 0
 
 def refine_sig(vuln_effect: list[InspectInfo], patch_effect: list[InspectInfo]) -> tuple[list[InspectInfo], list[InspectInfo]]:
@@ -143,34 +141,54 @@ def refine_sig(vuln_effect: list[InspectInfo], patch_effect: list[InspectInfo]) 
 
     for i in vuln_effect:
         if isinstance(i.ins, Effect.Call):
-            pair_dict["call"]["remove"].append(i)
+            pair_dict["call"]["remove"].append(i.ins)
         elif isinstance(i.ins, Effect.Condition):
-            pair_dict["condition"]["remove"].append(i)
+            pair_dict["condition"]["remove"].append(i.ins)
         elif isinstance(i.ins, Effect.Return):
-            pair_dict["return"]["remove"].append(i)
+            pair_dict["return"]["remove"].append(i.ins)
         elif isinstance(i.ins, Effect.Put):
-            pair_dict["put"]["remove"].append(i)
+            pair_dict["put"]["remove"].append(i.ins)
         elif isinstance(i.ins, Effect.Store):
-            pair_dict["store"]["remove"].append(i)
+            pair_dict["store"]["remove"].append(i.ins)
         else:
             print(f"Unknown effect type: {type(i.ins)}")
 
     for i in patch_effect:
         if isinstance(i.ins, Effect.Call):
-            pair_dict["call"]["add"].append(i)
+            pair_dict["call"]["add"].append(i.ins)
         elif isinstance(i.ins, Effect.Condition):
-            pair_dict["condition"]["add"].append(i)
+            pair_dict["condition"]["add"].append(i.ins)
         elif isinstance(i.ins, Effect.Return):
-            pair_dict["return"]["add"].append(i)
+            pair_dict["return"]["add"].append(i.ins)
         elif isinstance(i.ins, Effect.Put):
-            pair_dict["put"]["add"].append(i)
+            pair_dict["put"]["add"].append(i.ins)
         elif isinstance(i.ins, Effect.Store):
-            pair_dict["store"]["add"].append(i)
+            pair_dict["store"]["add"].append(i.ins)
         else:
             print(f"Unknown effect type: {type(i.ins)}")
 
-    print(f"pair_dict: {pair_dict}")
+    # print(f"pair_dict: {pair_dict}")
 
     # 1. calculate the similarity of each effect
+    
+    similarity_dict = {}
+    for type in info_type:
+        if len(pair_dict[type]["add"]) == 0 or len(pair_dict[type]["remove"]) == 0:
+            continue
+        for remove in pair_dict[type]["remove"]:
+            for add in pair_dict[type]["add"]:
+                score = similarity(remove, add, type)
+                if score > 0:
+                    key = str(remove)
+                    if key not in similarity_dict:
+                        similarity_dict[key] = (remove, add, score)
+                    else:
+                        # if score is greater than the current score, update it
+                        if similarity_dict[key][2] < score:
+                            similarity_dict[key] = (remove, add, score)
+                    # print(f"similarity_dict: {similarity_dict}")
+
+    # 2. refine the signature: 가장 높은 유사도를 가진 pair 들만 refine
+    print(f"similarity_dict: {similarity_dict}")
         
     return vuln_effect, patch_effect    
